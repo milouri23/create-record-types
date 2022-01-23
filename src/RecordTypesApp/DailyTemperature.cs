f5f26c2f009e90301b91f1b999102b79ea7de41a
Camilo Uribe Diaz <milouri23@gmail.com>
2022-01-23 13:09:15 -0500

    Primer commit

---
+++ src/RecordTypesApp/DailyTemperature.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RecordTypes.App
+{
+    public class DailyTemperatureClassic
+    {
+        public double HighTemp { get; }
+        public double LowTemp { get; }
+        public double Mean => (HighTemp + LowTemp) / 2.0;
+
+        public DailyTemperatureClassic(double highTemp, double lowTemp)
+        {
+            HighTemp = highTemp;
+            LowTemp = lowTemp;
+        }
+
+        // To string method that would generate the compiler
+        // If this was a record
+        public override string? ToString()
+        {
+            var builder = new StringBuilder();
+            builder.Append(nameof(DailyTemperatureClassic));
+            builder.Append(" { ");
+
+            if (PrintMembers(builder))
+                builder.Append(' ');
+
+            builder.Append('}');
+            return builder.ToString();
+        }
+
+        private bool PrintMembers(StringBuilder builder)
+        {
+            builder.Append(nameof(HighTemp));
+            builder.Append(" = ");
+            builder.Append(HighTemp);
+
+            builder.Append(", ");
+
+            builder.Append(nameof(LowTemp));
+            builder.Append(" = ");
+            builder.Append(LowTemp);
+
+            builder.Append(", ");
+
+            builder.Append(nameof(Mean));
+            builder.Append(" = ");
+            builder.Append(Mean);
+
+            return true;
+        }
+    }
+
+    //public readonly record struct DailyTemperature(double HighTemp, double LowTemp);
+
+    public readonly record struct DailyTemperature(double HighTemp, double LowTemp)
+    {
+        public double Mean => (HighTemp + LowTemp) / 2.0;
+    }
+
+    public abstract record DegreeDays(double BaseTemperature, IEnumerable<DailyTemperature> TempRecords)
+    {
+        protected virtual bool PrintMembers(StringBuilder builder)
+        {
+            builder.Append(nameof(BaseTemperature));
+            builder.Append(" = ");
+            builder.Append(BaseTemperature);
+
+            return true;
+        }
+    }
+
+    public sealed record HeatingDegreeDays(double BaseTemperature, IEnumerable<DailyTemperature> TempRecords)
+        : DegreeDays(BaseTemperature, TempRecords)
+    {
+        public double DegreesDays => TempRecords.Where(s => s.Mean < BaseTemperature).Sum(s => BaseTemperature - s.Mean);
+    }
+
+    public sealed record CoolingDegreeDays(double BaseTemperature, IEnumerable<DailyTemperature> TempRecords)
+        : DegreeDays(BaseTemperature, TempRecords)
+    {
+        public double DegreesDays => TempRecords.Where(s => s.Mean > BaseTemperature).Sum(s => s.Mean - BaseTemperature);
+    }
+}
---
+++ src/RecordTypesApp/Program.cs
@@ -0,0 +1,92 @@
+﻿using RecordTypes.App;
+using System;
+using System.Collections.Generic;
+
+DailyTemperature[] data = GetTemperatureData();
+
+foreach (var item in data)
+    Console.WriteLine(item);
+
+DailyTemperatureClassic[] dataClassic = GetTemperatureDataClassic();
+foreach (var item in dataClassic)
+    Console.WriteLine(item);
+
+Console.ReadLine();
+Console.WriteLine();
+
+var heatingDegreeDays = new HeatingDegreeDays(65, data);
+Console.WriteLine(heatingDegreeDays);
+
+var coolingDegreeDays = new CoolingDegreeDays(65, data);
+Console.WriteLine(coolingDegreeDays);
+
+// Growing degree days measure warming to determine plant growing rates
+var growingDegreeDays = coolingDegreeDays with { BaseTemperature = 41 };
+//Console.WriteLine(growingDegreeDays);
+
+//var growingDegreeDaysCopy = coolingDegreeDays with { };
+
+// showing moving accumlation of 5 days using range syntax
+List<CoolingDegreeDays> movingAccumulation = new();
+int rangeSize = (data.Length > 5) ? 5 : data.Length;
+for (int start = 0; start < data.Length - rangeSize; start++)
+{
+    var fiveDayTotal = growingDegreeDays with { TempRecords = data[start..(start + rangeSize)] };
+    movingAccumulation.Add(fiveDayTotal);
+}
+Console.WriteLine();
+Console.WriteLine("Total degree days in the last five days");
+foreach (var item in movingAccumulation)
+{
+    Console.WriteLine(item);
+}
+
+static DailyTemperature[] GetTemperatureData() =>
+    new DailyTemperature[]
+    {
+        new DailyTemperature(HighTemp: 57, LowTemp: 30),
+        new DailyTemperature(60, 35),
+        new DailyTemperature(63, 33),
+        new DailyTemperature(68, 29),
+        new DailyTemperature(72, 47),
+        new DailyTemperature(75, 55),
+        new DailyTemperature(77, 55),
+        new DailyTemperature(72, 58),
+        new DailyTemperature(70, 47),
+        new DailyTemperature(77, 59),
+        new DailyTemperature(85, 65),
+        new DailyTemperature(87, 65),
+        new DailyTemperature(85, 72),
+        new DailyTemperature(83, 68),
+        new DailyTemperature(77, 65),
+        new DailyTemperature(72, 58),
+        new DailyTemperature(77, 55),
+        new DailyTemperature(76, 53),
+        new DailyTemperature(80, 60),
+        new DailyTemperature(85, 66)
+    };
+
+static DailyTemperatureClassic[] GetTemperatureDataClassic() =>
+    new DailyTemperatureClassic[]
+    {
+        new DailyTemperatureClassic(highTemp: 57, lowTemp: 30),
+        new DailyTemperatureClassic(60, 35),
+        new DailyTemperatureClassic(63, 33),
+        new DailyTemperatureClassic(68, 29),
+        new DailyTemperatureClassic(72, 47),
+        new DailyTemperatureClassic(75, 55),
+        new DailyTemperatureClassic(77, 55),
+        new DailyTemperatureClassic(72, 58),
+        new DailyTemperatureClassic(70, 47),
+        new DailyTemperatureClassic(77, 59),
+        new DailyTemperatureClassic(85, 65),
+        new DailyTemperatureClassic(87, 65),
+        new DailyTemperatureClassic(85, 72),
+        new DailyTemperatureClassic(83, 68),
+        new DailyTemperatureClassic(77, 65),
+        new DailyTemperatureClassic(72, 58),
+        new DailyTemperatureClassic(77, 55),
+        new DailyTemperatureClassic(76, 53),
+        new DailyTemperatureClassic(80, 60),
+        new DailyTemperatureClassic(85, 66)
+    };
